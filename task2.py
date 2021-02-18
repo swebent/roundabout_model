7bfe5b8bae12a54910aadfc46cde77d77aab0ed9
swebent <alexander_benteby@hotmail.com>
2021-02-18 19:13:22 +0100

    distribution functions added control variables for the distributions at the top of the script

--- task2.py
+++ task2.py
@@ -1,6 +1,17 @@
 import random
 import simpy
 
+NORTH_RATE = 5
+WEST_RATE = 8
+SOUTH_RATE = 20
+EAST_RATE = 28
+
+ALPHA = 4#3.78
+BETA = 0.75 # 1.3#1.26
+
+QUEUE_DELAY_INTO = 0
+QUEUE_DELAY_ENTRY = 0
+NR_CARS = 0
 
 class Roundabout(object):
     def __init__(self, env, size):
@@ -12,8 +23,8 @@ class Roundabout(object):
         self.next_exit = ('Infinte', 9999)                           # { 'dir' }
         self.occupying_cars = []                        # { 'exit_dir' : time of exit }
 
-    # from car: ( entry_lane )
-    def request_enter_priority(self, entry_lane, name):
+
+    def request_enter_priority(self, entry_lane):
         with self.lock.request():
             n_exit = self.next_exit
 
@@ -28,17 +39,21 @@ class Roundabout(object):
                     exit_lane_number = 0
 
                 if exit_lane_number == entry_lane_number:
-                    print("car {}\t was given priority: ".format(name), priority)
                     return priority, n_exit[1]
 
                 exit_lane_number += 1
                 priority += 1
 
         else:  # if space
-            # print("car {}\t entry approved at EMPTY {}: ".format(name, env.now), self.space.count)
-            return 0, n_exit[1]
+            if n_exit is not ():
+                return 0, n_exit[1]
+            else:
+                return 0, env.now
 
-    # from car: ( drive_time, exit_lane, info )
+
+
+
+
     def enter(self, drive_time, exit_lane, name):
 
         with self.lock.request() as updater:
@@ -49,16 +64,17 @@ class Roundabout(object):
 
             self.occupying_cars.append(info)
 
-            if self.next_exit is not None:
+            if self.next_exit is not ():
                 (curr_dir, curr_time) = self.next_exit
 
                 if time < curr_time:
                     self.next_exit = info
             else:
+
                 self.next_exit = info
 
-        print("car {}\t entered at: {} ".format(name, env.now), "\tExit time is: ", time, "\tIt will exit at: ", exit_lane)
-        print("Next exit", self.next_exit)
+        #print("car {}\t entered at: {} ".format(name, env.now), "\tExit time is: ", time, "\tIt will exit at: ", exit_lane)
+        #print("Next exit", self.next_exit)
         return info
 
     # from car: ( info )
@@ -69,7 +85,7 @@ class Roundabout(object):
 
         with self.lock.request() as updater:
             # yield updater
-
+            car_list = self.occupying_cars.copy()
             self.occupying_cars.remove(info)
 
             for (i_dir, i_time) in self.occupying_cars:
@@ -82,47 +98,69 @@ class Roundabout(object):
             self.next_exit = temp
 
 
+
 def car(env, roundabout, entry_lane, exit_lane, drive_time, queue, name):
+    global QUEUE_DELAY_INTO, QUEUE_DELAY_ENTRY
     # print("car {}\tready at: ".format(name),env.now)
     arrive = env.now
 
     request = queue.request()                                       # place in queue to roundabout
     yield request
 
+    arrive_entry = env.now
+    QUEUE_DELAY_INTO += arrive_entry - arrive
+
     while True:
-        (priority_result, next_exit_time) = roundabout.request_enter_priority(entry_lane, name)
+        (priority_result, next_exit_time) = roundabout.request_enter_priority(entry_lane)
+
 
         with roundabout.space.request(priority=priority_result) as req:
+
             results = yield req | env.timeout(next_exit_time - env.now)
             if req in results:
                 info = roundabout.enter(drive_time, exit_lane, name)  # car has officially entered roundabout
-                # print(info)
-                queue_time = env.now - arrive
+
+                QUEUE_DELAY_ENTRY = env.now - arrive_entry
+
                 queue.release(request)
                 yield env.timeout(drive_time)
                 roundabout.exit(info, name)
-                print("car {}\t stood in queue for: ".format(name), queue_time)
+                #print("car {}\t stood in queue for: ".format(name), queue_time)
                 break
 
 
 def drive_time_calculator(from_lane, to_lane):
+    global GAMMA_TOT, GAMMA_NR
+    dir_converter = {'north': 1, 'west': 2, 'south': 3, 'east': 4}
+    multiplier = 1
+    entry_dir = dir_converter[from_lane] + 1
 
-    # const 5 sek in roundabout for all cars atm
 
-    return random.randint(2,10)
+    while multiplier < 4:
+        if entry_dir > 4:
+            entry_dir = 1
+
+        if entry_dir == dir_converter[to_lane]:
+            break
+
+        multiplier += 1
+
+    time = random.gammavariate(ALPHA, BETA)
+
+    #print(env.now ,"\t", 'drive time: {}'.format(time*multiplier))
+    return time*multiplier
 
 
 """
 this is the car generator. one will be created for each ingoing lane into the roundabout.
 traffic is a function which returns the current traffic on the lane
 """
-
-
 def source(env, traffic, destination, lane_name, roundabout, lane_queue):
+    global NR_CARS
 
     # unique traffic-func, destination-func, lane_name and lane_queue for each source process
-
-    for i in range(4):
+    i = 0
+    while True:
 
         name = "{}.".format(i) + lane_name
         # generate exit lane
@@ -132,36 +170,36 @@ def source(env, traffic, destination, lane_name, roundabout, lane_queue):
         drive_time = drive_time_calculator(lane_name, exit_lane)
 
         env.process(car(env, roundabout, lane_name, exit_lane, drive_time, lane_queue, name))
-
+        NR_CARS += 1
         # generate time between entering cars.
-        time = traffic(env.now)
+
+        time = random.expovariate(1 / traffic)
+
+        #print(env.now ,"\t", 'next arrival from {} at: {}'.format(lane_name,time))
         yield env.timeout(time)
 
-
-def traffic_func(time):
-    r = random.random()
-    t = r * 4 + 1
-    return t
+        i += 1
 
 
-def destination_func(from_lane):
-
-    l = random.choice(['north', 'west', 'south', 'east'])
-    if l == from_lane:
-        l = random.choice(['north', 'west', 'south', 'east'])
+def destination_func(lane):
+    destinations = ['north', 'west', 'south', 'east']
+    destinations.remove(lane)
+    l = random.choice(destinations)
+    #print(env.now ,"\t", 'from: {}, to:{}'.format(lane, l))
     return l
+
+
 
 
 env = simpy.Environment()
 
 rb = Roundabout(env, 4)
-n_queue = simpy.Resource(env, capacity=1)
-w_queue = simpy.Resource(env, capacity=1)
-s_queue = simpy.Resource(env, capacity=1)
-e_queue = simpy.Resource(env, capacity=1)
-env.process(source(env, traffic_func, destination_func, 'north', rb, n_queue))
-env.process(source(env, traffic_func, destination_func, 'south', rb, s_queue))
-env.process(source(env, traffic_func, destination_func, 'west', rb, w_queue))
-env.process(source(env, traffic_func, destination_func, 'east', rb, e_queue))
-env.run()
 
+env.process(source(env, NORTH_RATE, destination_func, 'north', rb, simpy.Resource(env, capacity=1)))
+env.process(source(env, SOUTH_RATE, destination_func, 'south', rb, simpy.Resource(env, capacity=1)))
+env.process(source(env, WEST_RATE, destination_func, 'west', rb, simpy.Resource(env, capacity=1)))
+env.process(source(env, EAST_RATE, destination_func, 'east', rb, simpy.Resource(env, capacity=1)))
+env.run(until=10000)
+
+print('queue delay before:',QUEUE_DELAY_INTO/NR_CARS)
+print('queue delay at entry:',QUEUE_DELAY_ENTRY/NR_CARS)
